ade6e383cae80ef17efe68a59d465209ce459856
Kajal Kotak <kajal.thobhani11@gmail.com>
2026-05-27 17:47:48 +0530

    change button name final

--- app/page.tsx
+++ app/page.tsx
@@ -423,7 +423,7 @@ Requirement: ${formData.message}`;
               }}
               className="bg-gradient-to-r from-[#5B0E74] to-[#F59E0B] text-white rounded-full py-5 text-xl font-bold hover:scale-[1.02] transition duration-300 shadow-lg"
             >
-              Free Book Consultation
+              Book Free Consultation
             </button>
           </form>
 
